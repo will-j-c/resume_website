adb9b289179921e6cf3b3f11adbc2837b71b7f2d
Will <willjcGA@outlook.com>
2023-11-17 00:05:21 +0800

    feat: added basic navbar

---
+++ src/app/ui/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div class='relative w-1/6 bg-white h-screen p-2'>
+      <h1 className='text-black text-3xl text-bold font-bold mb-3'>Will Colgate</h1>
+      <h5 className='text-black text-xs'>Head of Tax, Asia Pacific</h5>
+      <h5 className='text-black text-xs'>Chartered Accountant</h5>
+      <h5 className='text-black text-xs'>Aspiring Quant</h5>
+    </div>
+  );
+}
