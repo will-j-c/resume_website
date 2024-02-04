7367b5ec592001780181a074cd05f80e96599594
Will <willjcGA@outlook.com>
2024-02-04 13:19:56 +0800

    feat: added About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,12 @@
 import LeftCol from '@/app/ui/frames/LeftCol';
 import img from 'public/keming-tan-hGgM1F_Mq6g-unsplash.jpg';
+import About from '@/app/ui/About';
 
 // About Me page
 export default function Page() {
-  return <LeftCol props={{ bgImg: img, alt: 'MBS at night' }} />;
+  return (
+    <LeftCol props={{ bgImg: img, alt: 'MBS at night' }}>
+      <About />
+    </LeftCol>
+  );
 }
---
+++ src/app/ui/About.jsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <div className='flex h-full flex-col justify-center'>
+      <h1 className='text-lightThemeSecondaryText font-extrabold text-xl mb-5'>
+        Head of Tax
+      </h1>
+      <p className='mb-5 text-lightThemeSecondaryText text-sm'>
+        Dynamic and results-driven professional with extensive experience in
+        driving strategic tax initiatives and delivering impactful financial
+        outcomes to optimise organisational performance.
+      </p>
+      <p className='text-lightThemePrimaryText text-sm'>
+        Skilled in managing tax audits and disputes, negotiating favourable
+        resolutions, and mitigating tax risks. Adept at optimising tax
+        strategies, reducing costs, and ensuring regulatory compliance across
+        global markets. Proficient in addressing indirect tax matters, corporate
+        taxation, employee compensation & benefits taxation. Well-versed in tax
+        compliance, transfer pricing, tax planning, and tax risk management.
+        Known for successfully navigating complex tax landscapes and
+        implementing tax-efficient structures to improve bottom-line
+        performance. Expert in building and leading high-performing tax teams,
+        fostering professional development and implementing best practices.
+      </p>
+    </div>
+  );
+}
