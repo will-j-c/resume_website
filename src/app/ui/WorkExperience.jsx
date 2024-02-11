cd94113cbd7b48d90206b38c0bc9e4f18727a414
Will <willjcGA@outlook.com>
2024-02-11 15:41:35 +0800

    chore: added jobs

--- src/app/ui/WorkExperience.jsx
+++ src/app/ui/WorkExperience.jsx
@@ -3,7 +3,6 @@ import { arr } from '@/app/lib/jobs';
 
 export default function WorkExperience() {
   const jobs = arr;
-  console.log(jobs);
   return (
     <div className='flex h-full flex-col px-5 overflow-auto gap-5'>
       {jobs.map((job) => {
