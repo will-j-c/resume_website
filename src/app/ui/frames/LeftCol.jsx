834393ed7513833738cb369c539a135611e5bb0a
Will <willjcGA@outlook.com>
2024-02-18 13:48:07 +0800

    chore: removed suspense

--- src/app/ui/frames/LeftCol.jsx
+++ src/app/ui/frames/LeftCol.jsx
@@ -8,14 +8,13 @@ export default function LeftCol({ props, children }) {
     <div className='flex flex-col-reverse md:flex-row max-w-[1110px] mx-auto md:gap-36'>
       <div className='md:basis-7/12'>{children}</div>
       <div className='md:basis-5/12 relative mb-5 md:mb-0'>
-        (
         <Image
           src={bgImg}
           alt={alt}
           priority={true}
           className='invisible h-0 md:visible md:h-fit'
         />
-        );
+
         <Image
           src={bgImgMob}
           alt={alt}
