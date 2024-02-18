8f56f1d99a4b16c66c22995587c39761a3bfe5e8
Will <willjcGA@outlook.com>
2024-02-18 13:47:33 +0800

    chore: removed suspense

--- src/app/ui/frames/LeftCol.jsx
+++ src/app/ui/frames/LeftCol.jsx
@@ -8,24 +8,20 @@ export default function LeftCol({ props, children }) {
     <div className='flex flex-col-reverse md:flex-row max-w-[1110px] mx-auto md:gap-36'>
       <div className='md:basis-7/12'>{children}</div>
       <div className='md:basis-5/12 relative mb-5 md:mb-0'>
-        <Suspense fallback={<LargePhotoSkeleton />}>
-          (
-          <Image
-            src={bgImg}
-            alt={alt}
-            priority={true}
-            className='invisible h-0 md:visible md:h-fit'
-          />
-          );
-        </Suspense>
-        <Suspense fallback={<LargePhotoSkeleton />}>
-          <Image
-            src={bgImgMob}
-            alt={alt}
-            priority={true}
-            className='md:invisible md:h-0'
-          />
-        </Suspense>
+        (
+        <Image
+          src={bgImg}
+          alt={alt}
+          priority={true}
+          className='invisible h-0 md:visible md:h-fit'
+        />
+        );
+        <Image
+          src={bgImgMob}
+          alt={alt}
+          priority={true}
+          className='md:invisible md:h-0'
+        />
       </div>
     </div>
   );
